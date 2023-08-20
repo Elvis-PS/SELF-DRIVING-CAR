1801baff2878b14335dfc42eb8950f7ffcdafe98
Elvis Pontes <elvis-pontes@hotmail.com>
2023-08-20 10:07:01 -0300

    added dummy car

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ canvas.width= 240 ;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width*0.8);
 const car = new Car(road.getLaneCenter(1), 400, 30, 50);
+const traffic = [];
+traffic.push(new Car(road.getLaneCenter(1), 100, 30, 50));
 
 animate();
 
 function animate(){
     car.update(road.borders);
+    traffic.forEach(vehicle=>vehicle.update(road.borders));
     canvas.height = window.innerHeight;
 
     ctx.save();
@@ -16,7 +19,8 @@ function animate(){
 
     road.draw(ctx);
     car.draw(ctx);
-
+    traffic.forEach(vehicle=>vehicle.draw(ctx));
+    
     ctx.restore();
     requestAnimationFrame(animate);
 }
